5e52513c8a4958ae29016635c1ae1623e85f3c4c
Johan <bangjev190@gmail.com>
2025-06-01 21:07:12 +0700

    Change create db

--- app.py
+++ app.py
@@ -144,7 +144,8 @@ def summary():
         balance=income_total - expense_total
     )
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
