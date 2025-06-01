5a1310e8753ac81c07df306821d4bf19d747a2c5
Johan <bangjev190@gmail.com>
2025-06-01 20:31:48 +0700

    add CORS to req and app

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-# BudgetLite API dengan Flask + SQLite
-
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from flask_cors import CORS
 from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///budgetlite.db'
 app.config['JWT_SECRET_KEY'] = 'supersecretkey'
+CORS(app, origins=["https://vinsensiusjohan.github.io"])
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
